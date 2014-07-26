eea6d55287af43765a3aebc7b3aa5f98aa4ea148
Andrew Danger Lyon <orthecreedence@gmail.com>
2014-07-26 12:18:21 -0500

    doing some shadow stuff so better names can be used

--- nanomsg.lisp
+++ nanomsg.lisp
@@ -1,13 +1,38 @@
 (defpackage #:nanomsg
-  (:use :cl :cffi)
+  (:use :cffi)
+  (:import-from :cl
+                :in-package
+                :t :nil
+                :cons
+                :lambda :let :if :cond :case :prog :when :unless
+                :intern :export
+                :defmacro
+                :defun
+                :&body :&key :&optional :&aux
+                :labels
+                :eval-when
+                :loop :dotimes
+                :string :concatenate :symbol-name
+                :append
+                :*package*
+                :and :or
+                :setf
+                :defconstant
+                :null :upper-case-p :lower-case-p :digit-char-p :char-equal :string= :listp
+                :char-upcase :char-downcase
+                :> :>= :< :<=
+                :subseq
+                :nreverse :list :list* :length :car :cdr :first :second
+                :quote)
   (:nicknames :nn)
-  (:shadow :close))
+  (:shadow :close :symbol))
 
 (defpackage #:nanomsg.accessors
   (:use :cl :cffi :nanomsg)
   (:nicknames :nn-a)
   (:shadowing-import-from :nanomsg
-                          :close))
+                          :close
+                          :symbol))
 
 (in-package :nanomsg)
 
